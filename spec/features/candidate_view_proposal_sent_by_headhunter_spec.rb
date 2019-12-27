17e0e56d57174c05703c3a081608685927a88dc3
IcaroRSiqueira <icarorob@hotmail.com>
2019-12-27 00:21:05 -0300

    candidato visualiza proposta enviada por headhunter

--- spec/features/candidate_view_proposal_sent_by_headhunter_spec.rb
+++ spec/features/candidate_view_proposal_sent_by_headhunter_spec.rb
@@ -1,34 +1,30 @@
 require 'rails_helper'
 
-feature 'Headhunter send proposal to candidate' do
+feature 'Candidate view proposal' do
   scenario 'from home page' do
     headhunter = Headhunter.create!(email: 'test@test.com', password: '123456',
                                     name: 'Teste Enterprises')
-    candidate1 = Candidate.create!(email: 'test1@test.com', password: '123456', status: :complete)
-    profile1 = Profile.create!(full_name: 'Junior Silva', social_name: 'Leticia Silva',
+    candidate = Candidate.create!(email: 'test1@test.com', password: '123456', status: :complete)
+    profile = Profile.create!(full_name: 'Junior Silva', social_name: 'Leticia Silva',
                               birth_date: '13/12/1985', education: 'Graduação em ADS pela USP',
                               description: 'Curso finalizado em 2010',
-                              experience: '5 anos de desenvolvimento front end em ruby e java', candidate: candidate1)
+                              experience: '5 anos de desenvolvimento front end em ruby e java', candidate: candidate)
     vacancy = Vacancy.create!(title: 'Desenvolvedor Web', description: 'Desenvilvimento de paginas web com ruby on rails',
                     skill: 'Experiencia com ruby on rails', wage: '3000', role: 'Junior',
                     end_date: 15.day.from_now, location: 'Av Paulista', headhunter: headhunter)
-    Entry.create!(candidate: candidate1, vacancy: vacancy,
-                         description: 'Possuo bastante experiencia como desenvolvedor')
+    entry = Entry.create!(candidate: candidate, vacancy: vacancy,
+                  description: 'Possuo bastante experiencia como desenvolvedor', status: :rejected)
+    Proposal.create!(entry: entry, candidate: candidate, start_date: 20.day.from_now,
+                     workload: 'Segunda a sexta-feira, das 9 as 17h, totalizando 41 horas semanais',
+                     benefits: 'Vale transporte e alimentação', wage: 'R$ 3000,00 ao mês',
+                     details: 'A desenvolvedora deverá trabalhar junto a equipe de desenvolvimento adicionando as features exigidas ao sistema da empresa')
 
-    login_as(headhunter, scope: :headhunter)
+    login_as(candidate, scope: :candidate)
 
     visit root_path
 
-    click_on 'Aplicaçōes recebidas'
-    click_on 'Enviar proposta para candidato'
-    fill_in 'Data prevista para início', with: 20.day.from_now
-    fill_in 'Carga horária', with: 'Segunda a sexta-feira, das 9 as 17h, totalizando 41 horas semanais'
-    fill_in 'Benefícios', with: 'Vale transporte e alimentação'
-    fill_in 'Salário', with: 'R$ 3000,00 ao mês'
-    fill_in 'Detalhes', with: 'A desenvolvedora deverá trabalhar junto a equipe de desenvolvimento adicionando as features exigidas ao sistema da empresa'
-    click_on 'Enviar proposta'
+    click_on 'Propostas recebidas'
 
-    expect(page).to have_content("Proposta enviada!")
     expect(page).to have_content("Teste Enterprises")
     expect(page).to have_content("Leticia Silva")
     expect(page).to have_content(20.day.from_now.strftime('%d/%m/%Y'))
